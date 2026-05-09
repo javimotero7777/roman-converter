9bafeff5c7bc00cca4ec3eae7bde3236159e41fd
javimotero7777 <javimotero7777@gmail.com>
2026-05-09 15:51:33 +0200

    Refactor ESLint configuration for script.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,17 @@
 module.exports = [
-
-	{
-		"rules":{
-			"no-cond-assign":[
-				"error",
-				"always"
-			
-			],
-			"indent":[
-				"error",
-				2
-			
-			],
-			"linebreak-style":[
-				"error",
-				"unix"
-			
-			],
-			"quotes":[
-				"error",
-				"single"
-			],
-			"no-unused-vars": "off",
-			"semi": [
-				"error",
-				"always"
-			]
-
-		}
-
-	}
-
+  {
+    files: ['script.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'script'
+    },
+    rules: {
+      'no-cond-assign': ['error', 'always'],
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': 'off',
+      semi: ['error', 'always']
+    }
+  }
 ];
