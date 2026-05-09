9c01b640cacf1c2f21f32bf2e4ab217f32ba3a05
javimotero7777 <javimotero7777@gmail.com>
2026-05-09 15:42:36 +0200

    Revise ESLint config with new rules and format
    Updated ESLint configuration to use CommonJS module format and added various rules for code quality.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,34 @@
-export default [
-  {
-    files: ["script.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "script"
-    },
-    rules: {}
-  }
+module.exports = [
+
+	{
+		"rules":{
+			"no-cond-assign":[
+				"error",
+				"always"
+			
+			],
+			"indent":[
+				"error",
+				2
+			
+			],
+			"linebreak-style":[
+				"error",
+				"unix"
+			
+			],
+			"quotes":[
+				"error",
+				"single"
+			],
+			"no-unused-vars": "off",
+			"semi": [
+				"error",
+				"always"
+			]
+
+		}
+
+	}
+
 ];
